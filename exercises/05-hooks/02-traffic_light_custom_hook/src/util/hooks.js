efd7365dcd9d28f48af0451e5b50dca67d2c5f74
Erno <hentonen@gmail.com>
2021-02-12 11:56:35 +0200

    hooks exercises

---
+++ exercises/05-hooks/02-traffic_light_custom_hook/src/util/hooks.js
@@ -0,0 +1,7 @@
+import {useState,useEffect} from 'react';
+
+export const useLight = (initialState = "red") => {
+	
+	//TODO: using the existing hooks from the previous exercise create a custom hook that returns the color state to Trafficlight.
+
+}
